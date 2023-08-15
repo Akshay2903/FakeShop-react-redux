2111fd06dd53c0eeadc23ef783bdd42288f50cbf
Akshay Pal <akshaypal2903@gmail.com>
2023-08-15 18:26:34 +0530

    third commit

--- src/container/ProductList.js
+++ src/container/ProductList.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useCallback, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { ProductComponents } from './ProductComponents'
 import axios from 'axios'
@@ -10,19 +10,19 @@ export const ProductList = () => {
     const products = useSelector((state) => state)
     const dispatch = useDispatch()
 
-    const fetchProducts = async () => {
+    const fetchProducts = useCallback( async () => {
         const response = await axios
             .get("https://fakestoreapi.com/products")
             .catch((err) => {
                 console.log('Err', err)
             });
             dispatch(setProduct(response.data))
-    }
+    },[dispatch])
 
 
     useEffect(() => {
         fetchProducts();
-    },[])
+    },[fetchProducts])
 
     console.log("Products: ", products)
   return (
